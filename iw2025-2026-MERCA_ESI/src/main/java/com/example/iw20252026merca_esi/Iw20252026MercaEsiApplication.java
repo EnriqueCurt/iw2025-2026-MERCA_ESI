b18b6ed8bce23393c838d154950bef9685814c70
manuelgraya <manuel.garciaraya@alum.uca.es>
2025-11-11 18:45:50 +0100

    rama main ignora la base de datos, quitar lineas cuando base de datos este hecha

--- iw2025-2026-MERCA_ESI/src/main/java/com/example/iw20252026merca_esi/Iw20252026MercaEsiApplication.java
+++ iw2025-2026-MERCA_ESI/src/main/java/com/example/iw20252026merca_esi/Iw20252026MercaEsiApplication.java
@@ -2,8 +2,10 @@ package com.example.iw20252026merca_esi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Iw20252026MercaEsiApplication {
 
     public static void main(String[] args) {
